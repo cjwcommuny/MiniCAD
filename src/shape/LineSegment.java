d03127f91230ef9488538c53b39e937f947f837f
cjwcommuny <cjwcommuny@outlook.com>
2018-12-08 23:02:51 +0800

    bugs on line direction and numerical bugs fixes

--- src/shape/LineSegment.java
+++ src/shape/LineSegment.java
@@ -1,41 +1,84 @@
 package shape;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class LineSegment extends Shape {
-    private Point firstPoint;
-    private Point secondPoint;
+    private Point2D firstPoint;
+    private Point2D unitVector = new Point2D.Double(0, 0);
+    private double length;
 
     Point getFirstPoint() {
-        return firstPoint;
+        return new Point((int) firstPoint.getX(), (int) firstPoint.getY());
     }
 
     public Point getSecondPoint() {
-        return secondPoint;
+        int secondPointX;
+        int secondPointY;
+        if (unitVector.getX() == Double.POSITIVE_INFINITY
+                || unitVector.getX() == Double.NEGATIVE_INFINITY) {
+            secondPointX = (int) firstPoint.getX();
+        } else {
+            secondPointX = (int) (unitVector.getX() * length + firstPoint.getX());
+        }
+        if (unitVector.getY() == Double.POSITIVE_INFINITY || unitVector.getY() == Double.NEGATIVE_INFINITY) {
+            secondPointY = (int) firstPoint.getY();
+        } else {
+            secondPointY = (int) (unitVector.getY() * length + firstPoint.getY());
+        }
+        return new Point(secondPointX, secondPointY);
     }
 
     @Override
     public void render(Graphics2D imageGraphics, boolean isActivated) {
+//        System.out.println(unitVector.getX() + " " + unitVector.getY());
+//        System.out.println((unitVector.getY() * length) / (unitVector.getX() * length));
         super.render(imageGraphics, isActivated);
-        imageGraphics.drawLine(firstPoint.x, firstPoint.y, secondPoint.x, secondPoint.y);
+        Point secondPoint = getSecondPoint();
+        Point firstPointInt = getFirstPoint();
+        imageGraphics.drawLine(firstPointInt.x, firstPointInt.y, secondPoint.x, secondPoint.y);
     }
 
     public void setSecondPoint(Point secondPoint) {
-        this.secondPoint = secondPoint;
+        calculateLengthAndUnitVector(secondPoint);
+    }
+
+    private void calculateLengthAndUnitVector(Point secondPoint) {
+        length = Math.sqrt(
+                Math.pow(secondPoint.x - firstPoint.getX(), 2) +
+                Math.pow(secondPoint.y - firstPoint.getY(), 2)
+        );
+        double xDiff = secondPoint.x - firstPoint.getX();
+        double yDiff = secondPoint.y - firstPoint.getY();
+        double unitVectorX;
+        double unitVectorY;
+        if (xDiff == 0 && length == 0) {
+            unitVectorX = Double.POSITIVE_INFINITY;
+        } else {
+            unitVectorX = xDiff / length;
+        }
+        if (yDiff == 0 && length == 0) {
+            unitVectorY = Double.POSITIVE_INFINITY;
+        } else {
+            unitVectorY = yDiff / length;
+        }
+        unitVector.setLocation(unitVectorX, unitVectorY);
     }
 
     public LineSegment(Point firstPoint, Point secondPoint) {
         this.firstPoint = firstPoint;
-        this.secondPoint = secondPoint;
+        calculateLengthAndUnitVector(secondPoint);
     }
 
     @Override
     public boolean isInShape(Point point) {
         //TODO: complext shape frame
-        return point.x >= Math.min(firstPoint.x, secondPoint.x)
-                && point.x <= Math.max(firstPoint.x, secondPoint.x)
-                && point.y >= Math.min(firstPoint.y, secondPoint.y)
-                && point.y <= Math.max(firstPoint.y, secondPoint.y);
+        Point secondPoint = getSecondPoint();
+        Point firstPointInt = getFirstPoint();
+        return point.x >= Math.min(firstPointInt.x, secondPoint.x)
+                && point.x <= Math.max(firstPointInt.x, secondPoint.x)
+                && point.y >= Math.min(firstPointInt.y, secondPoint.y)
+                && point.y <= Math.max(firstPointInt.y, secondPoint.y);
     }
 
     @Override
@@ -49,18 +92,21 @@ public class LineSegment extends Shape {
 //        firstPoint.y = (int) ((firstPoint.x - center.x) * tan) + center.y;
 //        secondPoint.x = (int) ((secondPoint.x - center.x) * (1 + rate)) + center.x;
 //        secondPoint.y = (int) ((secondPoint.x - center.x) * tan) + center.y;
-        double tan = ((double) firstPoint.y - secondPoint.y) / (firstPoint.x - secondPoint.x);
-        double tempSecondPointX = firstPoint.x + (secondPoint.x - firstPoint.x) * (1 + rate);
-        secondPoint.y = firstPoint.y + (int) ((tempSecondPointX - firstPoint.x) * tan);
-        secondPoint.x = (int) tempSecondPointX;
-        System.out.println(tan);
+//        double tan = ((double) firstPoint.y - secondPoint.y) / (firstPoint.x - secondPoint.x);
+//        double tempSecondPointX = firstPoint.x + (secondPoint.x - firstPoint.x) * (1 + rate);
+//        secondPoint.y = firstPoint.y + (int) ((tempSecondPointX - firstPoint.x) * tan);
+//        secondPoint.x = (int) tempSecondPointX;
+//        System.out.println(tan);
+        length = length * (1 + rate);
+
     }
 
     @Override
     public void move(Point direction) {
-        firstPoint.x += direction.x;
-        firstPoint.y += direction.y;
-        secondPoint.x += direction.x;
-        secondPoint.y += direction.y;
+        firstPoint.setLocation(firstPoint.getX() + direction.x, firstPoint.getY() + direction.y);
+//        firstPoint.x += direction.x;
+//        firstPoint.y += direction.y;
+//        secondPoint.x += direction.x;
+//        secondPoint.y += direction.y;
     }
 }
